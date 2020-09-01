72f7fb732b62b447d89692bf376297e77e69ec42
Jeremy Richards <jeremyrichards2289@gmail.com>
2020-09-01 21:15:36 +0200

    cats model

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,5 @@
 class CatsController < ApplicationController
+  def index
+    @cats = Cat.all
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_29_133409) do
+ActiveRecord::Schema.define(version: 2020_09_01_184726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cats", force: :cascade do |t|
+    t.string "name"
+    t.string "gender"
+    t.string "breed"
+    t.boolean "allergies"
+    t.integer "user_id"
+    t.string "mood"
+    t.boolean "available"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
