53a2fcb79a8be6c51b21f8a5ab6d4593a8ca5869
Martin Lillebo <63665667+martinlillebo@users.noreply.github.com>
2020-09-08 21:05:12 +0200

    Revert "dashboard created"

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'cats#index'
-  resources :cats, only: [:index, :show, :new, :create]
-  get 'dashboard' , to: 'pages#dashboard'
-
+  resources :cats, only: [:index, :show, :new, :create, :dashboard]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
