52bef4a1686e3a599a4b688cc62658f72bc6ee93
Martin Lillebo <martin@pqa.no>
2020-09-10 21:27:21 +0200

    added calendar and controller mehod to create and store a cat booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,16 @@
 class BookingsController < ApplicationController
+  def create
+    @new_booking = Booking.new(strong_params)
+    @new_booking.user = current_user
+    @new_booking.cat = Cat.find(params[:cat_id])
+    @new_booking.save!
+    redirect_to root_path
+  end
+
+private
+
+def strong_params
+  params.require(:booking).permit(:start_date, :end_date)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'cats#index'
-  resources :cats, only: [:index, :show, :new, :create]
+  resources :cats, only: [:index, :show, :new, :create] do 
+    resources :bookings, only: [:create]
+  end
   get 'dashboard' , to: 'pages#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
