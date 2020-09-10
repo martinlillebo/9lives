7e373a79cf88373ba930d03f510d7ed50b39ef41
Leif Johansen <leif@leifjohansen.com>
2020-09-10 14:45:07 +0200

    fixing dashboard controller and dashboard.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'cats#index'
-  resources :cats, only: [:index, :show, :new, :create, :dashboard]
+  resources :cats, only: [:index, :show, :new, :create]
+  get 'dashboard' , to: 'pages#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
